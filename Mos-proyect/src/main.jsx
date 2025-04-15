36b691a41d7c0e6ddf951bf6a2eb0280ffa23fd4
Nana <ntlperezr@gmail.com>
2025-04-15 10:31:26 -0600

    Mos-proyect/package-lock.json

--- Mos-proyect/src/main.jsx
+++ Mos-proyect/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './styles/styles.css' // Asegúrate de que Tailwind esté aquí
+import './index.css'  // Asegúrate de tener esta línea
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
-
