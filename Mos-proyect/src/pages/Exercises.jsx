b944665290859557bde11ef6d9a587265be5b515
Nana <ntlperezr@gmail.com>
2025-04-21 16:56:31 -0600

    Mos-proyect/src/pages/Exercises.jsx

--- Mos-proyect/src/pages/Exercises.jsx
+++ Mos-proyect/src/pages/Exercises.jsx
@@ -1,4 +1,10 @@
 export default function Exercises() {
     return <div className="p-4 text-xl">Sección: Ejercicios</div>;
+    <p>
+    <li>Ejercicios interactivos: Preguntas de selección múltiple con retroalimentación inmediata. Ejercicios de "arrastrar y soltar" para relacionar conceptos. </li>
+    <li>Espacios de escritura guiada para reformular definiciones. </li>
+    <li>Selección de definiciones: Identificar la definición correcta de un término psicológico clave. </li>
+    <li>Retroalimentación: Corrección inmediata con explicaciones sobre respuestas incorrectas. Pistas y sugerencias mediante preguntas guía y opciones interactivas, sin dar respuestas directas. </li>
+    </p>
   }
   
--- Mos-proyect/src/pages/Miperfil.jsx
+++ Mos-proyect/src/pages/Miperfil.jsx
@@ -1,9 +1,15 @@
 const MiProgreso = () => {
     return (
       <div className="p-6">
-        <h1 className="text-3xl font-bold mb-4">Mi Progreso</h1>
+        <h1 className="text-3xl font-bold mb-4">Mi perfil</h1>
         <p>Aquí podrás visualizar tu avance en las lecciones, ejercicios y evaluaciones.</p>
-        {/* Aquí irán las gráficas, tablas e información personalizada */}
+        <h6>Personalización</h6>
+        <p>Aqui se presentaran las modificaciones al entorno de la interfaz:
+          <li>Color de fondo</li>
+          <li>Color de texto</li>
+          <li>Tipo de letra</li>
+        </p>
+        <p>Aquí irán las gráficas, tablas e información personalizada</p>
       </div>
     )
   }
